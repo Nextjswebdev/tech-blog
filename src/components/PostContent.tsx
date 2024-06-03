c2753cf9d5587d24e4b1052c2d12df9e5084f4fe
Mohd Khan <mohdkhan@Mohd-ka-MacBook-Air.local>
2024-06-03 11:55:06 +0530

    solve 2error

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -7,13 +7,13 @@ type PostContentProps = {
     content: string;
   };
   
-  const PostContent: React.FC<PostContentProps> = ({ title, date, content, image }) => {
+  const PostContent: React.FC<PostContentProps> = ({ title, date, content }) => {
     return (
       <div className="flex justify-center">
       <div className="w-4/5 lg:w-3/5 p-4 font-custom">
         <h1 className="text-2xl font-bold mb-2 text-gray-400">{title}</h1>
         <span className="text-sm text-gray-600">{date}</span>
-        <Image src={image} alt={title} width={800} height={400} className="rounded-lg mt-4" />
+        {/* <Image src={image} alt={title} width={800} height={400} className="rounded-lg mt-4" /> */}
         <div className="mt-4 text-gray-300" dangerouslySetInnerHTML={{ __html: content }}></div>
       </div>
     </div>
