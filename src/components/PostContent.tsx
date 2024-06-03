31891fc2bc15203fd2b07b31c298aa505a97a9ed
Mohd Khan <mohdkhan@Mohd-ka-MacBook-Air.local>
2024-06-03 07:56:50 +0530

    eslint0

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 // components/PostContent.tsx
 type PostContentProps = {
     title: string;
@@ -5,12 +7,13 @@ type PostContentProps = {
     content: string;
   };
   
-  const PostContent: React.FC<PostContentProps> = ({ title, date, content }) => {
+  const PostContent: React.FC<PostContentProps> = ({ title, date, content, image }) => {
     return (
       <div className="flex justify-center">
       <div className="w-4/5 lg:w-3/5 p-4 font-custom">
         <h1 className="text-2xl font-bold mb-2 text-gray-400">{title}</h1>
         <span className="text-sm text-gray-600">{date}</span>
+        <Image src={image} alt={title} width={800} height={400} className="rounded-lg mt-4" />
         <div className="mt-4 text-gray-300" dangerouslySetInnerHTML={{ __html: content }}></div>
       </div>
     </div>
